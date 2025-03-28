d32cec6fa2df81a83b2478557d38871520b8a831
Carlos Aguilar Miguel <nukiro@yahoo.com>
2025-03-28 15:26:20 +0100

    keep code

--- clients/frontend/console/views/application.py
+++ clients/frontend/console/views/application.py
@@ -1,8 +1,6 @@
 from tkinter import Tk
 from typing import Optional
 
-# from clients.frontend.console.views.cards import Cards
-# from clients.frontend.console.views.menu import Menu
 from clients.frontend.console.views.account import AccountView
 from clients.frontend.console.views.cards import CardsView
 from clients.frontend.console.views.games import GamesView
@@ -33,55 +31,14 @@ class Application(Tk):
         self.geometry(f"{settings.get('width')}x{settings.get('height')}")
         self.minsize(settings.get("width"), settings.get("height"))
 
+        # UI
         window = TabView(self)
 
         window.add(AccountView("Account", window.screen))
         window.add(CardsView("Cards", window.screen))
         window.add(GamesView("Games", window.screen))
 
-        # cards_view = Frame(window.screen)
-        # Label(cards_view, background="red", text="This is cards label").pack(
-        #     expand=True, fill="both"
-        # )
-        # cards: TabScreenView = dict(label="Cards", key="cards", content=cards_view)
-        # window.add(cards)
-
-        # games_view = Frame(window.screen)
-        # Label(games_view, background="yellow", text="This is games label").pack(
-        #     expand=True, fill="both"
-        # )
-        # games: TabScreenView = dict(label="Games", key="games", content=games_view)
-        # window.add(games)
-
         window.view("Account")
-
-        # Menu Bar
-        # menu = Frame(self)
-        # Button(menu, text="Account", command=lambda: content.show("account")).pack(
-        #     fill="x"
-        # )
-        # Button(menu, text="Cards", command=lambda: content.show("cards")).pack(fill="x")
-        # Button(menu, text="Games", command=lambda: content.show("games")).pack(fill="x")
-        # menu.place(relx=0, rely=0, relheight=1, width=100)
-
-        # content = TabScreenView(self)
-
-        # account = Frame(content)
-        # Label(account, background="green", text="This is account label").pack(
-        #     expand=True, fill="both"
-        # )
-        # content.add("account", account)
-
-        # cards = Frame(content)
-        # Label(cards, background="red", text="This is cards label").pack(expand=True)
-        # content.add("cards", cards)
-
-        # games = Frame(content)
-        # Label(games, background="blue", text="This is games label").pack(fill="both")
-        # content.add("games", games)
-
-        # content.show("account")
-        # content.place(x=100, rely=0, relheight=1, relwidth=1)
 
     def run(self):
         self.mainloop()
